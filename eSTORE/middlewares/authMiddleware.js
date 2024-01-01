6df27a5482ed29b4e6aff96add77fa4b96108f21
sukalyan Adhikary <sadhikary0055@gmail.com>
2024-01-01 17:44:35 +0530

    commit

--- eSTORE/middlewares/authMiddleware.js
+++ eSTORE/middlewares/authMiddleware.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const userModel=require('../models/userModel')
 
 //protecting routes according to the token
 const requireSignIn=async (req,res,next)=>{
@@ -7,6 +8,7 @@ const requireSignIn=async (req,res,next)=>{
             req.headers.authorization,
             process.env.JWT_SECRET
         );
+        req.user = decode ;
         next();
 
 
@@ -16,8 +18,27 @@ const requireSignIn=async (req,res,next)=>{
 }
 
 //for accessing the admin 
+const isAdmin=async (req,res,next) =>{
+    try{
+        const user= await userModel.findById(req.user._id) ;
+        if(user.role !== 1){
+            return res.status(401).send({
+                success:false,
+                message: "Unauthorized Acess",
+            });
+        }else{
+            next();
+        }
+
+    }catch(error){
+        res.status(401).send({
+            success:false,
+            error,
+            message:"Error in Admin Middleware",
+        })
+    }
+}
 
 
 
-
-module.exports=requireSignIn;
+module.exports={requireSignIn , isAdmin};
